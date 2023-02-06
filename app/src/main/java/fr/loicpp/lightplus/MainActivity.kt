c956b3b0e5e75fba9dadf1cab429a8981b8540df
Niskko <niskko.yt@gmail.com>
2023-02-06 23:19:12 +0100

    Add icon, first activity

--- app/src/main/java/fr/loicpp/lightplus/MainActivity.kt
+++ app/src/main/java/fr/loicpp/lightplus/MainActivity.kt
@@ -1,11 +1,46 @@
 package fr.loicpp.lightplus
 
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+    var flashLightStatus: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1.setOnClickListener(View.OnClickListener {
+            Log.d("DEBUG","Click !")
+            for(i in 0..10) {
+                openFlashLight()
+            }
+
+        })
+    }
+
+
+    private fun openFlashLight() {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        if (!flashLightStatus) {
+            try {
+                cameraManager.setTorchMode(cameraId, true)
+                flashLightStatus = true
+
+            } catch (e: CameraAccessException) {
+            }
+        } else {
+            try {
+                cameraManager.setTorchMode(cameraId, false)
+                flashLightStatus = false
+            } catch (e: CameraAccessException) {
+            }
+        }
     }
 }
