83e39c7761a1310de549a623d5d53efbaafffa5f
Niskko <niskko.yt@gmail.com>
2023-02-08 17:35:35 +0100

    Add blink function

--- app/src/main/java/fr/loicpp/lightplus/MainActivity.kt
+++ app/src/main/java/fr/loicpp/lightplus/MainActivity.kt
@@ -1,4 +1,4 @@
-package fr.loicpp.lightplus
+﻿package fr.loicpp.lightplus
 
 import android.content.Context
 import android.hardware.camera2.CameraAccessException
@@ -8,39 +8,64 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.concurrent.timer
+import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.time.toDuration
 
 class MainActivity : AppCompatActivity() {
     var flashLightStatus: Boolean = false
+    var isBlink: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn1.setOnClickListener(View.OnClickListener {
             Log.d("DEBUG","Click !")
-            for(i in 0..10) {
-                openFlashLight()
+            if(isBlink) {
+                isBlink=false
             }
-
+            else {
+                isBlink=true
+                blinkLight(100)
+            }
         })
     }
 
-
-    private fun openFlashLight() {
+    private fun blinkLight(Delay: Long = 100) {
+        if (this.isBlink) {
+            openFlashLight(null)
+            Timer().schedule(Delay.toLong()) {
+                openFlashLight(null)
+                Timer().schedule(Delay.toLong()) {
+                    blinkLight()
+                }
+            }
+        }
+    }
+    private fun openFlashLight(turnInto: Boolean?) {
+        if (turnInto!=null) {
+            if (turnInto) {
+                turnFLashLight(true)
+            }
+            else {
+                turnFLashLight(false)
+            }
+        }
+        else {
+            if (!flashLightStatus) {
+                turnFLashLight(true)
+            } else {
+                turnFLashLight(false)
+            }
+        }
+    }
+    private fun turnFLashLight(turnInto: Boolean) {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         val cameraId = cameraManager.cameraIdList[0]
-        if (!flashLightStatus) {
-            try {
-                cameraManager.setTorchMode(cameraId, true)
-                flashLightStatus = true
+        try {
+            cameraManager.setTorchMode(cameraId, turnInto)
+            flashLightStatus = turnInto
 
-            } catch (e: CameraAccessException) {
-            }
-        } else {
-            try {
-                cameraManager.setTorchMode(cameraId, false)
-                flashLightStatus = false
-            } catch (e: CameraAccessException) {
-            }
+        } catch (e: CameraAccessException) {
         }
     }
 }
